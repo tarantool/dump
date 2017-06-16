d4c2befe43507c8d3898559163c5027603b533b2
Konstantin Osipov <kostja@tarantool.org>
2017-06-16 16:57:55 +0300

    Enable tests.

--- dump/init.lua
+++ dump/init.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
---- Example of a Lua module for Tarantool
+--- Logical dump and restore for Tarantool
 --------------------------------------------------------------------------------
 
 --
@@ -12,20 +12,14 @@ local log = require('log') -- some other Tarantool module
 -- Constants
 --
 
--- local variables are only visible from this file
-local SOME_CONSTANT = 10
-
 --
 -- Internal functions
 --
 
--- Some internal function
-local function test(x)
-    if x == nil then
-        error("Usage: test(x: integer)")
-    end
-    log.info("test() called with x=%s", x)
-    return x + SOME_CONSTANT
+local function dump(path)
+end
+
+local function restore(path)
 end
 
 --
@@ -34,6 +28,7 @@ end
 
 -- result returned from require('dump')
 return {
-    test = test;
+    dump = dump;
+    restore = restore;
 }
 -- vim: ts=4 sts=4 sw=4 et
--- test/dump.test.lua
+++ test/dump.test.lua
@@ -1,14 +1,18 @@
 #!/usr/bin/env tarantool
 
-local kit = require('dump')
+local dump = require('dump')
 local tap = require('tap')
 
 local test = tap.test('dump tests')
+
 test:plan(1)
 
-test:test('dump', function(test)
-    test:plan(1)
-    test:is(kit.test(1), 11, "Lua function in init.lua")
-end)
+function basic(test)
+    test:plan(2)
+    test:is(type(dump.dump), "function", "Dump function is present")
+    test:is(type(dump.restore), "function", "Restore function is present")
+end
+
+test:test('basic', basic)
 
 os.exit(test:check() == true and 0 or -1)
